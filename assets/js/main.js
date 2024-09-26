c6789b3faa3e5088c11508884489a9e107e24b6a
Epikdimond-N <nathanael.pivot@ynov.com>
2024-09-26 14:35:46 +0200

    Closes #27

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 import { Game } from "./game.js";
 
 let game = new Game("element", 4);
+document.querySelectorAll(".tile-container2048").forEach((tile)=>{
+    if(!tile.classList.contains('tile')) {
+        tile.addEventListener("click", assignNeighboringTiles)
+    }
+})
 
 document.addEventListener('keydown', async (e) => {
     switch (e.key) {
@@ -26,6 +31,13 @@ document.addEventListener('keydown', async (e) => {
         document.querySelector('.game-message').classList.add(game.Win ? 'game-won' : 'game-over');
         document.querySelector('.game-message').querySelector('p').innerText = game.Win ? 'Vous avez gagné !' : 'Jeu terminé !';
     }
+    console.log(document.querySelector(".tile-container2048"))
+
+    document.querySelectorAll(".tile-container2048").forEach((tile)=>{
+        if(!tile.classList.contains('tile')) {
+            tile.addEventListener("click", assignNeighboringTiles)
+        }
+    })
 });
 
 const restartButtons = document.querySelectorAll('.restart-fonction');
@@ -56,4 +68,62 @@ function Normal(){
 
 function Reverse(){
     game = new Game("reverse", 4);
+}
+
+function assignNeighboringTiles(e){
+    const parentClass = e.target.parentElement;
+    const match = parentClass.className.match(/tile-position-(\d+)-(\d+)/);
+    let x, y
+    if (match) {
+        x = match[1] - 1;
+        y = match[2] - 1;
+        console.log(`x: ${x}, y: ${y}`);
+    }
+    parentClass.classList.add(game.Grid[y][x].element + "-cell-selected")
+    console.log(game.Grid[y][x])
+
+    if (y >= 0 && y < 3){
+        if(game.Grid[y+1][x].value !== 0){
+            document.querySelector(`.tile-position-${x+1}-${y+2}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
+        } // on check le bas
+        if (x >=0 && x<3){
+            if(game.Grid[y][x+1].value !== 0){
+                document.querySelector(`.tile-position-${x+2}-${y+1}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
+            } // on check à droite
+        }
+        if (x<=3 && x>0){
+            if(game.Grid[y][x-1].value !== 0){
+                document.querySelector(`.tile-position-${x}-${y+1}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
+            } // on check a gauche
+        }
+
+        if(y > 0 && y <=3){
+            if(game.Grid[y-1][x].value !== 0){
+                document.querySelector(`.tile-position-${x+1}-${y}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
+            } // on check le haut
+        }
+    }
+
+    if (y <= 3 && y > 0){
+        if(game.Grid[y-1][x].value !== 0){
+            document.querySelector(`.tile-position-${x+1}-${y}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
+        } // on check le bas
+        if (x >=0 && x<3){
+            if(game.Grid[y][x+1].value !== 0){
+                document.querySelector(`.tile-position-${x+2}-${y+1}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
+            } // on check à droite
+        }
+        if (x<=3 && x>0){
+            if(game.Grid[y][x-1].value !== 0){
+                document.querySelector(`.tile-position-${x}-${y+1}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
+            } // on check a gauche
+        }
+
+        if(y > 0 && y <=3){
+            if(game.Grid[y-1][x].value !== 0){
+                document.querySelector(`.tile-position-${x+1}-${y}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
+            } // on check le haut
+        }
+    }
+
 }
