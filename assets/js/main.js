aaba3af9958be6a6294df77223ffa56ce35f9a0c
Epikdimond-N <nathanael.pivot@ynov.com>
2024-09-26 23:45:56 +0200

    Closes #33

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
-import { Game } from "./game.js";
+import {Game} from "./game.js";
 import {commenceTimer, resetTimer, startTimer} from "./timer.js";
 
 let game = new Game();
 let isTimerSet = false; // Indicateur pour savoir si un temps a été sélectionné
+let lstTilesEvenListeners = []
 
-document.querySelectorAll(".tile-container2048").forEach((tile)=>{
-    if(!tile.classList.contains('tile')) {
+
+document.querySelectorAll(".tile-container2048").forEach((tile) => {
+    if (!tile.classList.contains('tile')) {
         tile.addEventListener("click", assignNeighboringTiles)
     }
 })
@@ -13,10 +15,16 @@ document.addEventListener('keydown', handleKeydown);
 
 function handleKeydown(e) {
     // Ne pas permettre de bouger tant que le timer n'est pas défini (minutes non sélectionnées)
-    if (!isTimerSet && game.Mode ==="chrono") return;
+    if (!isTimerSet && game.Mode === "chrono") return;
     document.querySelector('.tile-container2048').querySelectorAll("div").forEach((tile) => removeAnimation(tile))
     //document.querySelector('.tile-container2048').querySelectorAll("div").forEach((tile) => removeEvent(tile))
+    /*
+    lstTilesEvenListeners.forEach((tile) => {
+        document.querySelector(`tile-position-${tile.x + 1}-${tile.y + 1}`).removeEventListener('click', tile.UsePower)
+    })
 
+     */
+    lstTilesEvenListeners = [];
     switch (e.key) {
         case 'ArrowLeft':
             game.move('left');
@@ -55,8 +63,8 @@ function handleKeydown(e) {
         game.Win = null;
     });
 
-    document.querySelectorAll(".tile-container2048").forEach((tile)=>{
-        if(!tile.classList.contains('tile')) {
+    document.querySelector(".tile-container2048").querySelectorAll("div").forEach((tile) => {
+        if (!tile.classList.contains('tile') && !tile.classList.contains('tile-inner')) {
             tile.addEventListener("click", assignNeighboringTiles)
         }
     })
@@ -64,7 +72,7 @@ function handleKeydown(e) {
 
 // Gestion des clics sur les boutons pour le timer
 document.querySelectorAll('.container-btn-timer button').forEach(button => {
-    button.onclick = function() {
+    button.onclick = function () {
         const minutes = parseInt(this.innerText); // Récupérer les minutes à partir du texte du bouton
         startTimer(minutes); // Démarrer le timer à partir des minutes choisies
         isTimerSet = true; // Activer le mouvement une fois le temps sélectionné
@@ -84,123 +92,147 @@ function restartGame() {
     gameMessage.classList.remove('game-won', 'game-over');
     gameMessage.querySelector('p').innerText = '';
     resetTimer();
-    isTimerSet = false; // Réinitialiser l'indicateur de sélection de temps
+    isTimerSet = false;
 }
 
-function Element(){
+function Element() {
     game = new Game("element", 4);
 }
 
-function Chrono(){
+function Chrono() {
     game = new Game("chrono", 4);
 }
 
-function Normal(){
+function Normal() {
     game = new Game("normal", 4);
 }
 
-function Reverse(){
+function Reverse() {
     game = new Game("reverse", 4);
 }
 
-function assignNeighboringTiles(e){
+function assignNeighboringTiles(e) {
     document.querySelector('.tile-container2048').querySelectorAll("div").forEach((tile) => removeAnimation(tile))
 
     const parentClass = e.target.parentElement;
-    const match = parentClass.className.match(/tile-position-(\d+)-(\d+)/);
+    const match = parentClass?.className.match(/tile-position-(\d+)-(\d+)/);
     let x, y
     if (match) {
         x = match[1] - 1;
         y = match[2] - 1;
-        console.log(`x: ${x}, y: ${y}`);
     }
-    parentClass.classList.add(game.Grid[y][x].element + "-cell-selected")
-    console.log(game.Grid[y][x])
 
-    if (y >= 0 && y < 3){
-        if(game.Grid[y+1][x].value !== 0){
-            document.querySelector(`.tile-position-${x+1}-${y+2}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
-            document.querySelector(`.tile-position-${x+1}-${y+2}`).addEventListener("click", game.Grid[y][x].UsePower);
+
+    
+    if (y >= 0 && y < 3) {
+        if (game.Grid[y + 1][x].value !== 0) {
+            document.querySelector(`.tile-position-${x + 1}-${y + 2}`).classList.add(game.Grid[y][x].element + '-cell-selected-power');
+            document.querySelector(`.tile-position-${x + 1}-${y + 2}`).addEventListener("click",()=> {
+                game.Grid[y][x].UsePower(game.Grid[y + 1][x])
+            });
+
+            if (allUnique(lstTilesEvenListeners)) lstTilesEvenListeners.push(game.Grid[y][x])
         } // on check le bas
-        if (x >=0 && x<3){
-            if(game.Grid[y][x+1].value !== 0){
-                document.querySelector(`.tile-position-${x+2}-${y+1}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
-                document.querySelector(`.tile-position-${x+2}-${y+1}`).addEventListener("click", game.Grid[y][x].UsePower);
+        if (x >= 0 && x < 3) {
+            if (game.Grid[y][x + 1].value !== 0) {
+                document.querySelector(`.tile-position-${x + 2}-${y + 1}`).classList.add(game.Grid[y][x].element + '-cell-selected-power');
+                document.querySelector(`.tile-position-${x + 2}-${y + 1}`).addEventListener("click",()=> {
+                    game.Grid[y][x].UsePower(game.Grid[y][x + 1])
+                });
+                if (allUnique(lstTilesEvenListeners)) lstTilesEvenListeners.push(game.Grid[y][x])
 
             } // on check à droite
         }
-        if (x<=3 && x>0){
-            if(game.Grid[y][x-1].value !== 0){
-                document.querySelector(`.tile-position-${x}-${y+1}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
-                document.querySelector(`.tile-position-${x}-${y+1}`).addEventListener("click", game.Grid[y][x].UsePower);
+        if (x <= 3 && x > 0) {
+            if (game.Grid[y][x - 1].value !== 0) {
+                document.querySelector(`.tile-position-${x}-${y + 1}`).classList.add(game.Grid[y][x].element + '-cell-selected-power');
+                document.querySelector(`.tile-position-${x}-${y + 1}`).addEventListener("click",()=> {
+                    game.Grid[y][x].UsePower(game.Grid[y][x - 1])
+                });
+                if (allUnique(lstTilesEvenListeners)) lstTilesEvenListeners.push(game.Grid[y][x])
 
             } // on check a gauche
         }
 
-        if(y > 0 && y <=3){
-            if(game.Grid[y-1][x].value !== 0){
-                document.querySelector(`.tile-position-${x+1}-${y}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
-                document.querySelector(`.tile-position-${x+1}-${y}`).addEventListener("click", game.Grid[y][x].UsePower);
+        if (y > 0 && y <= 3) {
+            if (game.Grid[y - 1][x].value !== 0) {
+                document.querySelector(`.tile-position-${x + 1}-${y}`).classList.add(game.Grid[y][x].element + '-cell-selected-power');
+                document.querySelector(`.tile-position-${x + 1}-${y}`).addEventListener("click",()=> {
+                    game.Grid[y][x].UsePower(game.Grid[y - 1][x])
+                });
+                if (allUnique(lstTilesEvenListeners)) lstTilesEvenListeners.push(game.Grid[y][x])
 
             } // on check le haut
         }
     }
 
-    if (y <= 3 && y > 0){
-        if(game.Grid[y-1][x].value !== 0){
-            document.querySelector(`.tile-position-${x+1}-${y}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
-            document.querySelector(`.tile-position-${x+1}-${y}`).addEventListener("click", game.Grid[y][x].UsePower);
+    if (y <= 3 && y > 0) {
+        if (game.Grid[y - 1][x].value !== 0) {
+            document.querySelector(`.tile-position-${x + 1}-${y}`).classList.add(game.Grid[y][x].element + '-cell-selected-power');
+            document.querySelector(`.tile-position-${x + 1}-${y}`).addEventListener("click",()=> {
+                game.Grid[y][x].UsePower(game.Grid[y - 1][x])
+            });
+            if (allUnique(lstTilesEvenListeners)) lstTilesEvenListeners.push(game.Grid[y][x])
 
         } // on check le bas
-        if (x >=0 && x<3){
-            if(game.Grid[y][x+1].value !== 0){
-                document.querySelector(`.tile-position-${x+2}-${y+1}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
-                document.querySelector(`.tile-position-${x+2}-${y+1}`).addEventListener("click", game.Grid[y][x].UsePower);
+        if (x >= 0 && x < 3) {
+            if (game.Grid[y][x + 1].value !== 0) {
+                document.querySelector(`.tile-position-${x + 2}-${y + 1}`).classList.add(game.Grid[y][x].element + '-cell-selected-power');
+                document.querySelector(`.tile-position-${x + 2}-${y + 1}`).addEventListener("click",()=> {
+                    game.Grid[y][x].UsePower(game.Grid[y][x + 1])
+                });
+                if (allUnique(lstTilesEvenListeners)) lstTilesEvenListeners.push(game.Grid[y][x])
 
             } // on check à droite
         }
-        if (x<=3 && x>0){
-            if(game.Grid[y][x-1].value !== 0){
-                document.querySelector(`.tile-position-${x}-${y+1}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
-                document.querySelector(`.tile-position-${x}-${y+1}`).addEventListener("click", game.Grid[y][x].UsePower);
+        if (x <= 3 && x > 0) {
+            if (game.Grid[y][x - 1].value !== 0) {
+                document.querySelector(`.tile-position-${x}-${y + 1}`).classList.add(game.Grid[y][x].element + '-cell-selected-power');
+                document.querySelector(`.tile-position-${x}-${y + 1}`).addEventListener("click",()=> {
+                    game.Grid[y][x].UsePower(game.Grid[y][x - 1])
+                });
+                if (allUnique(lstTilesEvenListeners)) lstTilesEvenListeners.push(game.Grid[y][x])
 
             } // on check a gauche
         }
 
-        if(y > 0 && y <=3){
-            if(game.Grid[y-1][x].value !== 0){
-                document.querySelector(`.tile-position-${x+1}-${y}`).classList.add(game.Grid[y][x].element+'-cell-selected-power');
-                document.querySelector(`.tile-position-${x+1}-${y}`).addEventListener("click", game.Grid[y][x].UsePower);
+        if (y > 0 && y <= 3) {
+            if (game.Grid[y - 1][x].value !== 0) {
+                document.querySelector(`.tile-position-${x + 1}-${y}`).classList.add(game.Grid[y][x].element + '-cell-selected-power');
+                document.querySelector(`.tile-position-${x + 1}-${y}`).addEventListener("click",()=> {
+                    game.Grid[y][x].UsePower(game.Grid[y - 1][x])
+                });
+                if (allUnique(lstTilesEvenListeners)) lstTilesEvenListeners.push(game.Grid[y][x])
 
             } // on check le haut
         }
     }
-
-    document.querySelectorAll(".tile-container2048").forEach((tile)=>{
-        tile.removeEventListener('click', game.Grid[y][x].UsePower)
-    })
 }
 
-function removeAnimation(tile){
-        if(tile.classList.contains("flame-cell-selected-power")){
-            tile.classList.remove("flame-cell-selected-power");
-        } else if(tile.classList.contains("earth-cell-selected-power")){
-            tile.classList.remove("earth-cell-selected-power");
-        }else if(tile.classList.contains("wind-cell-selected-power")){
-            tile.classList.remove("wind-cell-selected-power");
-        }else if(tile.classList.contains("water-cell-selected-power")){
-            tile.classList.remove("water-cell-selected-power");
-        }else if(tile.classList.contains("flame-cell-selected")){
-            tile.classList.remove("flame-cell-selected");
-        } else if(tile.classList.contains("earth-cell-selected")){
-            tile.classList.remove("earth-cell-selected");
-        }else if(tile.classList.contains("wind-cell-selected")){
-            tile.classList.remove("wind-cell-selected");
-        }else if(tile.classList.contains("water-cell-selected")){
-            tile.classList.remove("water-cell-selected");
-        }
-
+function removeAnimation(tile) {
+    if (tile.classList.contains("flame-cell-selected-power")) {
+        tile.classList.remove("flame-cell-selected-power");
+    } else if (tile.classList.contains("earth-cell-selected-power")) {
+        tile.classList.remove("earth-cell-selected-power");
+    } else if (tile.classList.contains("wind-cell-selected-power")) {
+        tile.classList.remove("wind-cell-selected-power");
+    } else if (tile.classList.contains("water-cell-selected-power")) {
+        tile.classList.remove("water-cell-selected-power");
+    } else if (tile.classList.contains("flame-cell-selected")) {
+        tile.classList.remove("flame-cell-selected");
+    } else if (tile.classList.contains("earth-cell-selected")) {
+        tile.classList.remove("earth-cell-selected");
+    } else if (tile.classList.contains("wind-cell-selected")) {
+        tile.classList.remove("wind-cell-selected");
+    } else if (tile.classList.contains("water-cell-selected")) {
+        tile.classList.remove("water-cell-selected");
+    }
 }
 
-export { handleKeydown, restartGame, Element, Chrono, Normal, Reverse };
+function allUnique(arr) {
+    const uniqueSet = new Set(arr);
+    return uniqueSet.size === arr.length;
+}
+
+export {handleKeydown, restartGame, Element, Chrono, Normal, Reverse, lstTilesEvenListeners};
 
--- assets/js/tiles.js
+++ assets/js/tiles.js
@@ -1,3 +1,5 @@
+import {lstTilesEvenListeners} from "./main.js";
+
 export class Tiles{
     constructor(x, y, appear = false, value = 0, type = null){
         this.element = type;
@@ -7,7 +9,10 @@ export class Tiles{
         this.y = y
     }
 
-    UsePower(tileSelected){
+    UsePower(tileSelected = null ){
+       if (!lstTilesEvenListeners.includes(this)) return
+
+
         switch(this.element){
             case "flame":
                 this.UseFire(tileSelected);
@@ -22,18 +27,42 @@ export class Tiles{
                 this.UseWind(tileSelected);
                 break;
         }
-        this.element = null
 
     }
 
     UseFire(tileSelected){
         tileSelected.value = 0
         tileSelected.element = null
+        document.querySelector(`.tile-position-${tileSelected.x + 1}-${tileSelected.y + 1}`).remove();
+        document.querySelector(`.tile-position-${this.x + 1}-${this.y + 1}`).classList.remove(`${this.element}-cell`);
+        document.querySelector(`.tile-position-${this.x + 1}-${this.y + 1}`).classList.add("tile");
+        this.element = null
     }
 
     UseWater(tileSelected){
-        [this.x, tileSelected.x] = [tileSelected.x, this.x];
-        [this.y, tileSelected.y] = [tileSelected.y, this.y];
+        document.querySelector(`.tile-position-${tileSelected.x + 1}-${tileSelected.y + 1}`).classList.remove(`tile-${tileSelected.value}`)
+        document.querySelector(`.tile-position-${tileSelected.x + 1}-${tileSelected.y + 1}`).classList.add(`tile-${this.value}`)
+        document.querySelector(`.tile-position-${tileSelected.x + 1}-${tileSelected.y + 1}`).innerHTML = `<div class="tile-inner">${this.value}</div>`
+
+        document.querySelector(`.tile-position-${tileSelected.x + 1}-${tileSelected.y + 1}`).classList.remove(`${tileSelected.element}-cell`);
+        document.querySelector(`.tile-position-${tileSelected.x + 1}-${tileSelected.y + 1}`).classList.add("tile");
+
+
+        document.querySelector(`.tile-position-${this.x + 1}-${this.y + 1}`).classList.remove(`${this.element}-cell`);
+        document.querySelector(`.tile-position-${this.x + 1}-${this.y + 1}`).classList.add("tile");
+
+        if (tileSelected.element){
+            document.querySelector(`.tile-position-${this.x + 1}-${this.y + 1}`).classList.add(`${tileSelected.element}-cell`);
+            document.querySelector(`.tile-position-${this.x + 1}-${this.y + 1}`).classList.remove(`tile`)
+        }
+
+        document.querySelector(`.tile-position-${this.x + 1}-${this.y + 1}`).classList.remove(`tile-${this.value}`)
+        document.querySelector(`.tile-position-${this.x + 1}-${this.y + 1}`).classList.add(`tile-${tileSelected.value}`)
+        document.querySelector(`.tile-position-${this.x + 1}-${this.y + 1}`).innerHTML = `<div class="tile-inner">${tileSelected.value}</div>`;
+
+        [this.value, tileSelected.value] = [tileSelected.value, this.value];
+        this.element = tileSelected.element
+        tileSelected.element = null;
     }
 
     UseEarth(tileSelected){
